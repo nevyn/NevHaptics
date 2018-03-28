85bd1a11512983f0a386f4680f62756eaa749044
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2018-03-28 22:13:51 +0900

    Initial Commit

---
+++ NevHaptics/NevHaptics.h
@@ -0,0 +1,19 @@
+//
+//  NevHaptics.h
+//  NevHaptics
+//
+//  Created by Nevyn Bengtsson on 3/28/18.
+//  Copyright © 2018 Nevyn Bengtsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NevHaptics.
+FOUNDATION_EXPORT double NevHapticsVersionNumber;
+
+//! Project version string for NevHaptics.
+FOUNDATION_EXPORT const unsigned char NevHapticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NevHaptics/PublicHeader.h>
+
+
